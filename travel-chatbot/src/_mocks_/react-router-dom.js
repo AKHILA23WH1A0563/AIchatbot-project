32a96cf7f7e07716c0b662387d1a5841c8039fe5
AKHILA23WH1A0563 <150349875+AKHILA23WH1A0563@users.noreply.github.com>
2026-02-08 15:42:34 +0530

    Delete travel-chatbot directory

--- travel-chatbot/src/_mocks_/react-router-dom.js
+++
@@ -1,5 +0,0 @@
-export const BrowserRouter = ({ children }) => <div>{children}</div>;
-export const Routes = ({ children }) => <div>{children}</div>;
-export const Route = () => <div>Login</div>;
-export const Link = ({ children }) => <span>{children}</span>;
-export const useNavigate = () => jest.fn();
