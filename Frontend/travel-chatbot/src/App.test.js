30f498c5cef0001f62927cd62e4f98bcf56bb5ef
23WH1A1252 <23wh1a1252@bvrithyderabad.edu.in>
2026-02-26 21:55:55 +0530

    Removed all files

--- Frontend/travel-chatbot/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders login page by default", () => {
-  render(<App />);
-  expect(
-    screen.getByPlaceholderText("Email or Mobile Number")
-  ).toBeInTheDocument();
-});
